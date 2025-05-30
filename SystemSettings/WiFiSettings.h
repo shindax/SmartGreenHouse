61622a9ca9c8290b62d592f477c943526dc47776
Шендаков Станислав Геннадьевич <ShendakovSG@mikron.okb>
2025-05-30 09:38:47 +0700

    First commit. WiFiSettings.h added

---
+++ SystemSettings/WiFiSettings.h
@@ -0,0 +1,16 @@
+#ifdef WORK_WI_FI
+#define SSID      "OOO OKB Mikron"; 
+#define PASSWORD  "7d2_495N"; 
+#elif HOME_WI_FI
+#define SSID      "shAsus";
+#define PASSWORD  "StanislavShendakov7036";
+#elif GREENHOUSE_WI_FI
+#define SSID      "MERCUSYS_666C";
+#define PASSWORD  "79701846";
+#elif MY_REDME_WI_FI
+#define SSID      "shRedmeNote";
+#define PASSWORD  "shindax110";
+#else
+  #error "WI-FI point must be defined"
+#endif
+
