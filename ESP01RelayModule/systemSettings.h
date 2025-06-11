f45c53eb32ed11ed2f5554d8464095f09f363135
Шендаков Станислав Геннадьевич <ShendakovSG@mikron.okb>
2025-06-11 14:26:30 +0700

    Simple ESP01relayModule added.

---
+++ ESP01RelayModule/systemSettings.h
@@ -0,0 +1,11 @@
+#ifndef SYSTEM_SETTINGS_H
+#define SYSTEM_SETTINGS_H
+
+#include "d:\Projects\Arduino\SmartGreenHouse\System\wiFiSettings.h"
+#include "d:\Projects\Arduino\SmartGreenHouse\System\wifiFunctions.cpp"
+#include "d:\Projects\Arduino\SmartGreenHouse\System\boards.h"
+#include "d:\Projects\Arduino\SmartGreenHouse\System\boards.cpp"
+#include "d:\Projects\Arduino\SmartGreenHouse\System\functions.h"
+#include "d:\Projects\Arduino\SmartGreenHouse\System\functions.cpp"
+
+#endif
